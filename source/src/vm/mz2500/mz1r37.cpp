102f986b0d7c857babb7f11b81704577a5317c38
issaUt <issaUt@users.noreply.github.com>
2026-04-26 16:02:13 +0900

    Sync MZ-1R37 debugger support with Takeda implementation

--- source/src/vm/mz2500/mz1r37.cpp
+++ source/src/vm/mz2500/mz1r37.cpp
@@ -8,14 +8,13 @@
 */
 
 #include "mz1r37.h"
-
-#define EMM_SIZE	(640 * 1024)
+#include "../fileio.h"
 
 namespace MZ2500 {
 
 void MZ1R37::initialize()
 {
-	buffer = (uint8_t*)calloc(EMM_SIZE, sizeof(uint8_t));
+	buffer = (uint8_t*)calloc(MZ1R37_EMM_SIZE, sizeof(uint8_t));
 	address = 0;
 }
 
@@ -34,7 +33,7 @@ void MZ1R37::write_io8(uint32_t addr, uint32_t data)
 		break;
 	case 0xad:
 		address = (address & 0xffff00) | (addr >> 8);
-		if(address < EMM_SIZE) {
+		if(address < MZ1R37_EMM_SIZE) {
 			buffer[address] = data;
 		}
 		break;
@@ -46,13 +45,41 @@ uint32_t MZ1R37::read_io8(uint32_t addr)
 	switch(addr & 0xff) {
 	case 0xad:
 		address = (address & 0xffff00) | (addr >> 8);
-		if(address < EMM_SIZE) {
+		if(address < MZ1R37_EMM_SIZE) {
 			return buffer[address];
 		}
 		break;
 	}
 	return 0xff;
 }
+
+#ifdef USE_DEBUGGER
+uint64_t MZ1R37::get_debug_data_addr_space()
+{
+	return MZ1R37_EMM_SIZE;
+}
+
+void MZ1R37::write_debug_data8(uint32_t addr, uint32_t data)
+{
+	if(addr < MZ1R37_EMM_SIZE) {
+		buffer[addr] = data;
+	}
+}
+
+uint32_t MZ1R37::read_debug_data8(uint32_t addr)
+{
+	if(addr < MZ1R37_EMM_SIZE) {
+		return buffer[addr];
+	}
+	return 0;
+}
+
+bool MZ1R37::get_debug_regs_info(_TCHAR *buffer_text, size_t buffer_len)
+{
+	my_stprintf_s(buffer_text, buffer_len, _T("Address = %04Xxx\n"), (address & 0xffff00) >> 8);
+	return true;
+}
+#endif
 
 #define STATE_VERSION	1
 
@@ -64,8 +91,9 @@ bool MZ1R37::process_state(FILEIO* state_fio, bool loading)
 	if(!state_fio->StateCheckInt32(this_device_id)) {
 		return false;
 	}
-	state_fio->StateArray(buffer, EMM_SIZE, 1);
+	state_fio->StateArray(buffer, MZ1R37_EMM_SIZE, 1);
 	state_fio->StateValue(address);
 	return true;
 }
+
 }
--- source/src/vm/mz2500/mz1r37.h
+++ source/src/vm/mz2500/mz1r37.h
@@ -14,6 +14,8 @@
 #include "../../emu.h"
 #include "../device.h"
 
+#define MZ1R37_EMM_SIZE	(640 * 1024)
+
 namespace MZ2500 {
 
 class MZ1R37 : public DEVICE
@@ -21,7 +23,6 @@ class MZ1R37 : public DEVICE
 private:
 	uint8_t* buffer;
 	uint32_t address;
-	int tmp_buffer_size;
 public:
 	MZ1R37(VM_TEMPLATE* parent_vm, EMU_TEMPLATE* parent_emu) : DEVICE(parent_vm, parent_emu)
 	{
@@ -34,9 +35,18 @@ public:
 	void release() override;
 	void __FASTCALL write_io8(uint32_t addr, uint32_t data) override;
 	uint32_t __FASTCALL read_io8(uint32_t addr) override;
+#ifdef USE_DEBUGGER
+	bool is_debugger_available() override
+	{
+		return true;
+	}
+	uint64_t get_debug_data_addr_space() override;
+	void write_debug_data8(uint32_t addr, uint32_t data) override;
+	uint32_t read_debug_data8(uint32_t addr) override;
+	bool get_debug_regs_info(_TCHAR *buffer, size_t buffer_len) override;
+#endif
 	bool process_state(FILEIO* state_fio, bool loading) override;
 };
 
 }
 #endif
-
